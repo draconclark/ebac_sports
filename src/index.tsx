a4cfb947237404ca501c424591a915d98f675320
draconclark <124631437+draconclark@users.noreply.github.com>
2023-10-19 17:44:28 -0300

    index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { createGlobalStyle } from 'styled-components'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export const cores = {
+  corTexto: '#121714',
+  corFundo: '#eee',
+  corPrincipal: '#0c2461',
+  corSecundaria: '#4a69bd'
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    color: ${cores.corTexto}
+  }
+
+  body {
+    background-color: ${cores.corFundo};
+    padding-bottom: 80px;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+
+    @media (max-width: 1024px) {
+      max-width: 80%;
+    }
+  }
+`
